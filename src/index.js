68f9913ef7131d3217dd25fd74b7d15b1188352c
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-05-05 16:11:21 +0300

    js-3-11

--- src/index.js
+++ src/index.js
@@ -15,8 +15,44 @@ let gallery = new SimpleLightbox('.gallery a',{
     captionDelay: 250,
 });
 
+function createPictures(arr){
+    return arr
+    .map(
+    ({
+        webformatURL,
+        largeImageURL, 
+        tags, 
+        likes, 
+        views,
+        comments, 
+        downloads, 
+    }) => 
+    `<div class="photo-card">
+    <a class="link-img" href="${largeImageURL}">
+    <img src="${webformatURL}" alt="${tags}" width="300px" height="200px" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b> ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>${downloads}
+      </p>
+    </div></a>
+  </div>`
+    )
+    .join('');
+}
+
 form.addEventListener('submit', onSearch);
+
 function onSearch(evt){
+    
     evt.preventDefault();
     const {searchQuery} = evt.currentTarget.elements;
 
@@ -34,50 +70,64 @@ function onSearch(evt){
             observer.observe(target);
             gallery.refresh();
             Notify.info(`Hooray! We found ${totalHits} images.`);
+            
+        const { height: cardHeight } = document
+            .querySelector('.gallery')
+            .firstElementChild.getBoundingClientRect();
+          
+          window.scrollBy({
+            top: cardHeight * 2,
+            behavior: 'smooth',
+          });
         }
         if (total === history.length){
             observer.unobserve(target);
             return Notify.failure(
                 `We're sorry, but you've reached the end of search results.`
-            )
+            );
         }
     })
     .catch(err => console.log(err));
-
-
 } 
 
+let options = {
+    root: null,
+    rootMargin: '200px',
+    threshold: 1.0,
+};
+ 
+let observer = new IntersectionObserver(onLoad, options);
+function onLoad(entries, observer){
+    console.log(entries);
+    entries.forEach(entry => {
+        if (entry.isIntersecting){
+            currentPage += 1;
+            
+            fetchPictures(form.searchQuery.value, currentPage)
+            .then(datas => {
+                const {hits} = datas.data;
+                containerGallery.insertAdjacentHTML('beforeend', createCards(hits));
+                observer.observe(target);
 
-function createPictures(arr){
-    return arr
-    .map(
-    ({
-        webformatURL,
-        largeImageURL, 
-        tags, 
-        likes, 
-        views,
-        comments, 
-        downloads, 
-    }) => 
-    `<div class="photo-card">
-    < a class="link-img" href="${largeImageURL}">
-    <img src="${webformatURL}" alt="${tags}" width="300px" height="200px" loading="lazy" />
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b> ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>${downloads}
-      </p>
-    </div>
-  </div>`
-    )
-    .join('');
+                if (hits.length < 40){
+                    observer.unobserve(target);
+                    Notify.failure(
+                        "We're sorry, but you've reached the end of search results."
+                    );
+                }
+                gallery.refresh();
+
+                })
+                .catch(err => {
+                    if (err.status = 400){
+                        Notify.failure(
+                            "We're sorry, but you've reached the end of search results."
+                        );
+                    }
+                });
+            
+        }
+    });
+
 }
+
